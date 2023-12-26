07d461e90748ea36003095cda9a0731c2e3abf25
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2023-12-26 21:06:44 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,12 +1,84 @@
+# from flask import Flask, request, jsonify
+# import os
+# import threading
+# import dg
+# import os.path
+# import asyncio
+
+# app = Flask(__name__)
+# tasks = {}
+
+# def process_audio(file_path, task_id):
+#     try:
+#         transcripts = asyncio.run(dg.main_transcription()) 
+#         tasks[task_id] = {'result': transcripts}
+#     except Exception as e:
+#         tasks[task_id] = {'result': str(e)}
+
+# @app.route('/upload', methods=['POST'])
+# def upload_file():
+#     if 'file' not in request.files:
+#         return jsonify({'error': 'No file part'}), 400
+
+#     file = request.files['file']
+
+#     if file.filename == '':
+#         return jsonify({'error': 'No selected file'}), 400
+
+#     # Retrieve the task ID from the request
+#     task_id = request.form.get('id')
+#     if not task_id:
+#         return jsonify({'error': 'Task ID is required'}), 400
+
+#     if file:
+#         # Extract extension from the original file name
+#         _, file_extension = os.path.splitext(file.filename)
+
+#         # Use task_id as the name but keep the original extension
+#         filename = os.path.join('recordings', f"{task_id}{file_extension}")
+#         file.save(filename)
+
+#         tasks[task_id] = {'result': None}
+
+#         thread = threading.Thread(target=process_audio, args=(filename, task_id))
+#         thread.start()
+
+#         return jsonify({'task_id': task_id}), 202
+
+# @app.route('/result/<task_id>', methods=['GET'])
+# def get_result(task_id):
+#     if task_id not in tasks:
+#         return jsonify({'error': 'Invalid task ID'}), 404
+
+#     result = tasks[task_id]
+
+#     if 'result' not in result or result['result'] is None:
+#         return jsonify({'status': 'Processing'}), 202
+#     else:
+#         return jsonify({'result': result['result']}), 200
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 from flask import Flask, request, jsonify
 import os
 import threading
 import dg
-import os.path
+import requests
+from urllib.parse import urlparse
+import boto3
 import asyncio
+from flask import Flask, request, jsonify
+import os.path
 
 app = Flask(__name__)
 tasks = {}
+
+def download_file(url, local_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(local_path, 'wb') as f:
+            f.write(response.content)
 
 def process_audio(file_path, task_id):
     try:
@@ -15,6 +87,33 @@ def process_audio(file_path, task_id):
     except Exception as e:
         tasks[task_id] = {'result': str(e)}
 
+@app.route('/upload_url', methods=['POST'])
+def upload_recording_url():
+    data = request.json
+    if not data or 'file' not in data:
+        return jsonify({'error': 'No file URL provided'}), 400
+
+    file_url = data['file']
+    task_id = data.get('id')
+    file_type = data.get('type', 'call recording')  # Default to 'call recording' if not provided
+
+    if not task_id:
+        return jsonify({'error': 'ID is required'}), 400
+
+    parsed_url = urlparse(file_url)
+    file_name = os.path.basename(parsed_url.path)
+    _, file_extension = os.path.splitext(file_name)
+
+    local_filename = os.path.join('recordings', f"{task_id}{file_extension}")
+    download_file(file_url, local_filename)
+
+    tasks[task_id] = {'result': None}
+
+    thread = threading.Thread(target=process_audio, args=(local_filename, task_id))
+    thread.start()
+
+    return jsonify({'task_id': task_id}), 202
+    
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
@@ -28,7 +127,7 @@ def upload_file():
     # Retrieve the task ID from the request
     task_id = request.form.get('id')
     if not task_id:
-        return jsonify({'error': 'Task ID is required'}), 400
+        return jsonify({'error': 'ID is required'}), 400
 
     if file:
         # Extract extension from the original file name
@@ -44,6 +143,7 @@ def upload_file():
         thread.start()
 
         return jsonify({'task_id': task_id}), 202
+
 
 @app.route('/result/<task_id>', methods=['GET'])
 def get_result(task_id):
