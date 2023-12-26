f27a7c0479aa59c485d4d00d7347696f863f32b0
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2023-12-26 23:12:01 +0530

    Update dg.py

--- dg.py
+++ dg.py
@@ -1,4 +1,3 @@
-# connecting to mongo for status
 from deepgram import Deepgram
 import asyncio, os, sys, shutil
 from pymongo import MongoClient
@@ -54,49 +53,114 @@ def move_processed_file(filename):
         os.makedirs(COMPLETED_FOLDER)
     shutil.move(os.path.join(FOLDER_PATH, filename), os.path.join(COMPLETED_FOLDER, filename))
 
-# Async main function
-async def main_transcription():
+import os
+import asyncio
+import shutil
+from deepgram import Deepgram
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Your Deepgram API Key
+DEEPGRAM_API_KEY = os.environ.get('DEEPGRAM_API_KEY')
+
+# Folders for storing recordings and transcripts
+FOLDER_PATH = 'recordings'
+TRANSCRIPTIONS_FOLDER = 'transcriptions'
+COMPLETED_FOLDER = 'recordings_processed'
+
+# ... [Other functions remain unchanged] ...
+
+# Async main function with filename argument
+async def main_transcription(target_filename):
     deepgram = Deepgram(DEEPGRAM_API_KEY)
     transcripts = {}
 
-    # Iterating over each file in the folder
-    for filename in os.listdir(FOLDER_PATH):
-        if filename.endswith('.wav'):  # Check if the file is a WAV file
-            file_path = os.path.join(FOLDER_PATH, filename)
-        # elif filename.endswith('.mp3'):  # Check if the file is a WAV file
-        #     file_path = os.path.join(FOLDER_PATH, filename)
-            print(f"Transcribing {filename}...")
+    # Check if the target file exists in the folder
+    if target_filename in os.listdir(FOLDER_PATH) and target_filename.endswith('.wav'):
+        file_path = os.path.join(FOLDER_PATH, target_filename)
+        print(f"Transcribing {target_filename}...")
 
-            with open(file_path, 'rb') as audio:
-                source = {
-                    'buffer': audio,
-                    'mimetype': 'audio/wav'
-                }
-                options = {
-                    'smart_format': True,
-                    'diarize': True  # Enable diarization
-                }
-                try:
-                    response = await asyncio.create_task(
-                        deepgram.transcription.prerecorded(source, options)
-                    )
+        with open(file_path, 'rb') as audio:
+            source = {
+                'buffer': audio,
+                'mimetype': 'audio/wav'
+            }
+            options = {
+                'smart_format': True,
+                'diarize': True  # Enable diarization
+            }
+            try:
+                response = await asyncio.create_task(
+                    deepgram.transcription.prerecorded(source, options)
+                )
 
-                    # Handle the response with diarization data
-                    words_array = response["results"]["channels"][0]["alternatives"][0]["words"]
+                # Handle the response with diarization data
+                words_array = response["results"]["channels"][0]["alternatives"][0]["words"]
 
-                    # Process the words array
-                    transcript = format_transcript(words_array)
-                    transcripts[filename] = transcript
-                    print(transcript)
-                    print(f"Transcript for {filename} added.")
+                # Process the words array
+                transcript = format_transcript(words_array)
+                transcripts[target_filename] = transcript
+                print(transcript)
+                print(f"Transcript for {target_filename} added.")
 
-                    # Move the processed file
-                    move_processed_file(filename)
+                # Move the processed file
+                move_processed_file(target_filename)
 
-                    # Update MongoDB entry
-                    # update_mongodb_entry(filename, client)
+                # Update MongoDB entry (if applicable)
+                # update_mongodb_entry(target_filename, client)
 
-                except Exception as e:
-                    print(f"Error transcribing {filename}: {e}")
+            except Exception as e:
+                print(f"Error transcribing {target_filename}: {e}")
+    else:
+        print(f"File {target_filename} not found or not a WAV file.")
 
     return transcripts
+
+## Transcribing all the files in a folder
+# # Async main function
+# async def main_transcription():
+#     deepgram = Deepgram(DEEPGRAM_API_KEY)
+#     transcripts = {}
+
+#     # Iterating over each file in the folder
+#     for filename in os.listdir(FOLDER_PATH):
+#         if filename.endswith('.wav'):  # Check if the file is a WAV file
+#             file_path = os.path.join(FOLDER_PATH, filename)
+#         # elif filename.endswith('.mp3'):  # Check if the file is a WAV file
+#         #     file_path = os.path.join(FOLDER_PATH, filename)
+#             print(f"Transcribing {filename}...")
+
+#             with open(file_path, 'rb') as audio:
+#                 source = {
+#                     'buffer': audio,
+#                     'mimetype': 'audio/wav'
+#                 }
+#                 options = {
+#                     'smart_format': True,
+#                     'diarize': True  # Enable diarization
+#                 }
+#                 try:
+#                     response = await asyncio.create_task(
+#                         deepgram.transcription.prerecorded(source, options)
+#                     )
+
+#                     # Handle the response with diarization data
+#                     words_array = response["results"]["channels"][0]["alternatives"][0]["words"]
+
+#                     # Process the words array
+#                     transcript = format_transcript(words_array)
+#                     transcripts[filename] = transcript
+#                     print(transcript)
+#                     print(f"Transcript for {filename} added.")
+
+#                     # Move the processed file
+#                     move_processed_file(filename)
+
+#                     # Update MongoDB entry
+#                     # update_mongodb_entry(filename, client)
+
+#                 except Exception as e:
+#                     print(f"Error transcribing {filename}: {e}")
+
+#     return transcripts
