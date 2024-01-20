cc4e14d3a2c35e428854dff572230cabcc182bf9
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2024-01-20 14:22:59 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,64 +1,3 @@
-# from flask import Flask, request, jsonify
-# import os
-# import threading
-# import dg
-# import os.path
-# import asyncio
-
-# app = Flask(__name__)
-# tasks = {}
-
-# def process_audio(file_path, task_id):
-#     try:
-#         transcripts = asyncio.run(dg.main_transcription()) 
-#         tasks[task_id] = {'result': transcripts}
-#     except Exception as e:
-#         tasks[task_id] = {'result': str(e)}
-
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     if 'file' not in request.files:
-#         return jsonify({'error': 'No file part'}), 400
-
-#     file = request.files['file']
-
-#     if file.filename == '':
-#         return jsonify({'error': 'No selected file'}), 400
-
-#     # Retrieve the task ID from the request
-#     task_id = request.form.get('id')
-#     if not task_id:
-#         return jsonify({'error': 'Task ID is required'}), 400
-
-#     if file:
-#         # Extract extension from the original file name
-#         _, file_extension = os.path.splitext(file.filename)
-
-#         # Use task_id as the name but keep the original extension
-#         filename = os.path.join('recordings', f"{task_id}{file_extension}")
-#         file.save(filename)
-
-#         tasks[task_id] = {'result': None}
-
-#         thread = threading.Thread(target=process_audio, args=(filename, task_id))
-#         thread.start()
-
-#         return jsonify({'task_id': task_id}), 202
-
-# @app.route('/result/<task_id>', methods=['GET'])
-# def get_result(task_id):
-#     if task_id not in tasks:
-#         return jsonify({'error': 'Invalid task ID'}), 404
-
-#     result = tasks[task_id]
-
-#     if 'result' not in result or result['result'] is None:
-#         return jsonify({'status': 'Processing'}), 202
-#     else:
-#         return jsonify({'result': result['result']}), 200
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 
 from flask import Flask, request, jsonify
 import os
@@ -79,11 +18,13 @@ tasks = {}
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
 MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME")
 MONGO_DB_COLLECTION = os.environ.get("MONGO_DB_COLLECTION_REC")
+MONGO_DB_COLLECTION_MEETING = os.environ.get("MONGO_DB_COLLECTION_MEETING")
 
 # Set up MongoDB connection
 client = MongoClient(MONGO_DB_URI)
 db = client[MONGO_DB_NAME]
 collection = db[MONGO_DB_COLLECTION]
+collection_meeting = db[MONGO_DB_COLLECTION_MEETING]
 
 def download_file(url, local_path):
     response = requests.get(url)
@@ -170,30 +111,22 @@ def get_result(task_id):
     else:
         return jsonify({'result': result['result']}), 200
 
+
+@app.route('/meeting_url', methods=['POST'])
+def meeting_url():
+    data = request.json
+    link = data.get('link')
+    id = data.get('id')
+
+    if not link or not type:
+        return jsonify({"error": "Missing data"}), 400
+
+    # Insert data into MongoDB
+    collection_meeting.insert_one({"link": link, "id": id})
+
+    return jsonify({"message": "Data stored successfully"}), 201
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-# # adding 'type' parameter
-# @app.route('/result/<task_id>/<task_type>', methods=['GET'])
-# def get_result(task_id, task_type):
-#     if task_id not in tasks:
-#         return jsonify({'error': 'Invalid task ID'}), 404
-
-#     # Check if the task_type is valid
-#     if task_type not in ['callRecording', 'meeting']:
-#         return jsonify({'error': 'Invalid task type'}), 400
-
-#     result = tasks[task_id]
-
-#     # Check if the task type matches
-#     if result.get('type') != task_type:
-#         return jsonify({'error': 'Task type does not match'}), 400
-
-#     if 'result' not in result or result['result'] is None:
-#         return jsonify({'status': 'Processing', 'type': task_type}), 202
-#     else:
-#         return jsonify({'result': result['result'], 'type': task_type}), 200
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
